d7334771c291b444ae1ecfc3ec5efb901f94d259
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-01-01 22:33:20 +0000

    - Prepped for beta 1 build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'events';
-$app['version'] = '1.4.12';
+$app['version'] = '1.4.14';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
