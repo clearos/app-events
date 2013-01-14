041c721777c418afb854b89250b0a65aea2b3ff1
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-01-14 16:38:57 +0000

    - Changed log comment

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'events';
-$app['version'] = '1.4.14';
+$app['version'] = '1.4.15';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-events.spec
+++ packaging/app-events.spec
@@ -1,7 +1,7 @@
 
 Name: app-events
 Epoch: 1
-Version: 1.4.14
+Version: 1.4.15
 Release: 1%{dist}
 Summary: Event System - Core
 License: LGPLv3
