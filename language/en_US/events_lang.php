adc3c60e13305f4682ef3e8466c446f1e3a413cd
pcbaldwin <pbaldwin@clearfoundation.com>
2014-05-08 20:37:27 -0400

    Prepped for build

--- language/en_US/events_lang.php
+++ language/en_US/events_lang.php
@@ -1,4 +1,4 @@
 <?php
 
 $lang['events_app_name'] = 'Event System';
-$lang['events_app_description'] = 'The Event System app provides a way for other apps to listen to events that occur on the system';
+$lang['events_app_description'] = 'The Event System app provides a way for other apps to listen for events that occur on the system';
