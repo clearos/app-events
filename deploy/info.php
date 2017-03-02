deead6abad959224560bf28ff5dbfeaea9aa07b7
pcbaldwin <peter@egloo.ca>
2017-03-02 11:28:12 -0500

    Prepped for translations rebuild

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'events';
-$app['version'] = '2.3.20';
+$app['version'] = '2.3.22';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
